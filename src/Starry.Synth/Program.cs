b0300165f061b74214b3b7b26fe8a06e61c21cd2
ilfirin_nar <ilfirin.nar@gmail.com>
2015-10-22 22:00:18 +0300

    First commit

---
+++ src/Starry.Synth/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NAudio.Wave;
+
+namespace StarrySynth
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var sineWaveProvider = new SineWaveProvider32();
+            sineWaveProvider.SetWaveFormat(16000, 1); // 16kHz mono
+            sineWaveProvider.Frequency = 200;
+            sineWaveProvider.Amplitude = 0.25f;
+            var waveOut = new WaveOut
+            {
+                DesiredLatency = 100
+            };
+            waveOut.Init(sineWaveProvider);
+            waveOut.Play();
+            try
+            {
+                while (true)
+                {
+                    var keyInfo = Console.ReadKey();
+                    switch (keyInfo.Key) {
+                        case ConsoleKey.UpArrow:
+                            sineWaveProvider.Frequency += 100;
+                            break;
+                        case ConsoleKey.DownArrow:
+                            sineWaveProvider.Frequency -= 100;
+                            break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad frequency!");
+                Console.ReadKey();
+            }
+            finally
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+            }
+        }
+    }
+}
---
+++ src/Starry.Synth/WaveProvider32.cs
@@ -0,0 +1,32 @@
+﻿using NAudio.Wave;
+
+namespace StarrySynth
+{
+    public abstract class WaveProvider32 : IWaveProvider
+    {
+        protected WaveProvider32() : this(44100, 1) {}
+
+        private WaveProvider32(int sampleRate, int channels)
+        {
+            SetWaveFormat(sampleRate, channels);
+        }
+
+        public void SetWaveFormat(int sampleRate, int channels)
+        {
+            WaveFormat = WaveFormat.CreateIeeeFloatWaveFormat(sampleRate, channels);
+        }
+
+        public int Read(byte[] buffer, int offset, int count)
+        {
+            var waveBuffer = new WaveBuffer(buffer);
+            const int someDeriver = 4;
+            var samplesRequired = count / someDeriver;
+            var samplesRead = Read(waveBuffer.FloatBuffer, offset / someDeriver, samplesRequired);
+            return samplesRead * someDeriver;
+        }
+
+        protected abstract int Read(float[] buffer, int offset, int sampleCount);
+
+        public WaveFormat WaveFormat { get; private set; }
+    }
+}
