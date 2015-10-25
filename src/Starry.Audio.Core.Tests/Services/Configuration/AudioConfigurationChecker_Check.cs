8415f347221e76c66ae50caad16e44449c999654
ilfirin_nar <ilfirin.nar@gmail.com>
2015-10-26 00:48:21 +0300

    Add empty DAW projects

--- src/Starry.Audio.Core.Tests/Services/Configuration/AudioConfigurationChecker_Check.cs
+++ src/Starry.Audio.Core.Tests/Services/Configuration/AudioConfigurationChecker_Check.cs
@@ -7,18 +7,6 @@ namespace Starry.Audio.Core.Tests.Services.Configuration
         [Fact]
         public void PassingTest()
         {
-            Assert.Equal(4, Add(2, 2));
-        }
-
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
-        int Add(int x, int y)
-        {
-            return x + y;
         }
     }
 }
