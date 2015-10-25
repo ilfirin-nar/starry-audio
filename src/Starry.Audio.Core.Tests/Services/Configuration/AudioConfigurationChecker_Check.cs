65278da9de4cf86767a8efa6201fd845fe3ec5ec
ilfirin_nar <ilfirin.nar@gmail.com>
2015-10-26 00:25:54 +0300

    Add test for test the tests %)

---
+++ src/Starry.Audio.Core.Tests/Services/Configuration/AudioConfigurationChecker_Check.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace Starry.Audio.Core.Tests.Services.Configuration
+{
+    public class AudioConfigurationChecker_Check
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
